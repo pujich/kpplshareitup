656a811b458720a83b6cae4a0ef64353a04845bc
Unknown <muchammadandhikas@gmail.com>
2017-12-09 20:25:26 +0700

    perbaikan view
    perbaikan view

--- application/views/Profile.php
+++ application/views/Profile.php
@@ -78,10 +78,6 @@
 			</section> -->
 			<?php foreach ($data as $x) { ?>
 
-				</div>
-			</div><!-- /row -->
-	    </div> <!-- /container -->
-	</div><!-- /headerwrap -->
 
 			<section class="header_text sub">
 			<!-- <img class="pageBanner" src="<?php echo base_url("assets/img/GBP.jpg"); ?>" alt="Share-It-Up" > --><div class="col-md-12 ">
@@ -100,22 +96,9 @@
 			</section> -->
 			<section class="main-content">				
 				<div class="row">				
-				<!-- 	<div class="span5">
-						<div>
-							<h5>ADDITIONAL INFORMATION</h5>
-							<p><strong>Phone:</strong>&nbsp;(123) 456-7890<br>
-							<strong>Fax:</strong>&nbsp;+04 (123) 456-7890<br>
-							<strong>Email:</strong>&nbsp;<a href="#">vietcuong_it@yahoo.com</a>								
-							</p>
-							<br/>
-							<h5>SECONDARY OFFICE IN VIETNAM</h5>
-							<p><strong>Phone:</strong>&nbsp;(113) 023-1125<br>
-							<strong>Fax:</strong>&nbsp;+04 (113) 023-1145<br>
-							<strong>Email:</strong>&nbsp;<a href="#">vietcuong_it@yahoo.com</a>					
-							</p>
-						</div>
-					</div>  --><center>
-					<div class="col-md-12 	"> 
+				
+					<div class="span12"> 
+						<center>
 						<p>Nama Lengkap</p>
 						<h4><?php echo $x['nama_user']; ?></h4>
 						<p>Alamat</p>
@@ -126,9 +109,10 @@
 						<h4><?php echo $x['deskripsi']; ?></h4>
 						<!-- Tombol untuk menampilkan modal-->
 						<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Edit Profil</button>
-						</center>
+				
 						<?php } ?>
 						</form>
+						</center>
 					</div>
 					<!-- Modal -->
 					<div id="myModal" class="modal fade" role="dialog">
@@ -174,9 +158,7 @@
 						</div>
 					</div>
 				</div>
-			</section>	
-
-
+			</section>
 			<section id="footer-bar">
 				<div class="row">
 					<div class="span3">
